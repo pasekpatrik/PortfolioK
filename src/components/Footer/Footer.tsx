dfb7cfa29509b4e6061bebe67296f3407f9bcc63
Patrik Pasek <patrik.pasek@firma.seznam.cz>
2023-08-15 13:29:18 +0200

    odkazy sidebar footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -8,9 +8,9 @@ const Footer: React.FC = () => {
     return (
         <>
             <div className="social-footer">
-                <a href=""><FontAwesomeIcon icon={faLinkedin} /></a>
-                <a href=""><FontAwesomeIcon icon={faInstagram} /></a>
-                <a href=""><FontAwesomeIcon icon={faEnvelope} /></a>
+                <a href="https://www.linkedin.com/in/kate%C5%99ina-kortanov%C3%A1-30a864287" target='_blank'><FontAwesomeIcon icon={faLinkedin} /></a>
+                <a href="https://instagram.com/kajda.kortanka?utm_source=qr&igshid=MzNlNGNkZWQ4Mg%3D%3D" target='_blank'><FontAwesomeIcon icon={faInstagram} /></a>
+                <a href="mailto:kata.kortanova@seznam.cz"><FontAwesomeIcon icon={faEnvelope} /></a>
             </div>
             <span>&copy; Copyright 2023 <a href="https://portfoliopatrik.netlify.app/" target='_blank'>portfoliopatrik.netlify.app</a> | všechna práva vyhrazena </span>
         </>
--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -7,9 +7,9 @@ import './SideBar.css'
 const SideBar: React.FC = () => {
     return (
         <ul className="social">
-            <li><a href="" target="_blank"><span>Linkedin</span><span><FontAwesomeIcon icon={faLinkedin} /></span></a></li>
-            <li><a href="" target="_blank"><span>Instagram</span><span><FontAwesomeIcon icon={faInstagram} /></span></a></li>
-            <li><a href=""><span>Email</span><span><FontAwesomeIcon icon={faEnvelope} /></span></a></li>
+            <li><a href="https://www.linkedin.com/in/kate%C5%99ina-kortanov%C3%A1-30a864287" target="_blank"><span>Linkedin</span><span><FontAwesomeIcon icon={faLinkedin} /></span></a></li>
+            <li><a href="https://instagram.com/kajda.kortanka?utm_source=qr&igshid=MzNlNGNkZWQ4Mg%3D%3D" target="_blank"><span>Instagram</span><span><FontAwesomeIcon icon={faInstagram} /></span></a></li>
+            <li><a href="mailto:kata.kortanova@seznam.cz"><span>Email</span><span><FontAwesomeIcon icon={faEnvelope} /></span></a></li>
         </ul>
     )
 }
